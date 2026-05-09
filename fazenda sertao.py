bddce8e248287ab98c131f319134e1bb3ca852dd
franciscojonataslimadesouza7-svg <franciscojonataslimadesouza7@gmail.com>
2026-05-09 19:02:13 -0300

    Update fazenda sertao.py

--- fazenda sertao.py
+++ fazenda sertao.py
@@ -1,19 +1,5 @@
 # LOGIN
-while login == usuarios[0][1]:
-    print("bem vindo ADM")
 
-while login == usuarios[2][3]:
-    print("bem vindo CLIENTE")
-
-if opcao == "1":
-    while True:
-        login = input("Login: ")
-        senha = input("Senha: ")
-        credencial = input("Credencial (ADM ou CLIENTE): ")
-        usuarios.append([login, senha, credencial])
-
-        print("Usuário cadastrado com sucesso!")
-        break
 
 
 
